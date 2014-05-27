7a02da82b7cf98734b3a00c70d2b3c5dd291736f
Marcus J. Hildum <marcus.hildum@gmail.com>
2014-05-26 19:04:37 -0700

    fix formatting

--- JavaClassNameGenerator.pl
+++ JavaClassNameGenerator.pl
@@ -5,48 +5,48 @@ use strict;
 use warnings;
 
 my @nonsense = qw(
-        Abstract
-        Actor
-        Adapter
-        Bean
-        Builder
-        Facade
-        Factory
-        Generator
-        Global
-        Handler
-        Injector
-        Interceptor
-        Interface
-        Iterator
-        Lazy
-        Listener
-        Loader
-        Manager
-        Mediator
-        Observer
-        Parser
-        Pool
-        Prototype
-        Scheduler
-        Serializer
-        Service
-        Servlet
-        Session
-        Singleton
-        Strategy
-        Tokenizer
-        Util
-        Visitor
-        Wrapper
-        XML
+	Abstract
+	Actor
+	Adapter
+	Bean
+	Builder
+	Facade
+	Factory
+	Generator
+	Global
+	Handler
+	Injector
+	Interceptor
+	Interface
+	Iterator
+	Lazy
+	Listener
+	Loader
+	Manager
+	Mediator
+	Observer
+	Parser
+	Pool
+	Prototype
+	Scheduler
+	Serializer
+	Service
+	Servlet
+	Session
+	Singleton
+	Strategy
+	Tokenizer
+	Util
+	Visitor
+	Wrapper
+	XML
 );
 
 my $rand = int(rand(scalar @nonsense - 1));
 my @class_name;
 
-for (0..$rand){
-        push @class_name, $nonsense[rand @nonsense];
+for (0..$rand) {
+	push @class_name, $nonsense[rand @nonsense];
 }
 
 say @class_name;
